a2c01c8ba26c91c4f8912e95229369e588ee5a0b
mattwobh <mwhunt@lakeheadu.ca>
2021-02-28 22:08:58 -0500

    Main class complete, GroceryItem instance variables and constructor complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,2 +1,12 @@
 public class GroceryItem {
+
+    private String item;
+    private double price;
+    private String type;
+
+    public GroceryItem(String item, double price, String type){
+        setName(item);
+        setPrice(price);
+        setCategory(type);
+    }
 }
