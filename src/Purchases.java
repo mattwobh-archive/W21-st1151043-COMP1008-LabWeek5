42436832643d748584e46549bd0b0aed82fd9fc8
mattwobh <mwhunt@lakeheadu.ca>
2021-02-28 23:46:25 -0500

    Purchases complete.

--- src/Purchases.java
+++ src/Purchases.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+
 public class Purchases {
-    public Purchases(){
-        String[] itemArray;
+    private ArrayList<GroceryItem> itemArray = new ArrayList<>();
+    private double totalPrice;
+    public void addGroceryItem(GroceryItem item){
+        itemArray.add(item);
+    }
+    public double getTotalPrice(){
+        totalPrice = 0;
+        for(int i = 0; i < itemArray.size(); i++) {
+            totalPrice += itemArray.get(i).getPrice();
+        }
+        return totalPrice;
+    }
+    public String getGroceryNames(){
+        String names = "";
+        for(int i = 0; i < itemArray.size(); i++) {
+            names += itemArray.get(i).getName() + ", ";
+        }
+        return names;
+    }
+    public String getCategories(){
+        String categories = "";
+        for(int i = 0; i < itemArray.size(); i++) {
+            categories += itemArray.get(i).getCategory() + ", ";
+        }
+        return categories;
+    }
+    public String toString(){
+        return "The grocery list has " + itemArray.size() + " with a total cost of $" + (double)Math.round(totalPrice*100)/100 + ".";
     }
 }
